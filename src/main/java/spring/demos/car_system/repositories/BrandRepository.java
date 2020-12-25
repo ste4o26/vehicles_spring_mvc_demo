659942dc17c087c5b6486cfbe859f3769858f8bf
Stefan <36564989+ste4o26@users.noreply.github.com>
2020-12-25 19:33:21 +0000

    one method added in both BrandRepository and BrandService interfaces

--- src/main/java/spring/demos/car_system/repositories/BrandRepository.java
+++ src/main/java/spring/demos/car_system/repositories/BrandRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import spring.demos.car_system.domain.entities.Brand;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByCreated(LocalDateTime dateTime);
 }
--- src/main/java/spring/demos/car_system/services/interfaces/BrandService.java
+++ src/main/java/spring/demos/car_system/services/interfaces/BrandService.java
@@ -1,5 +1,10 @@
 package spring.demos.car_system.services.interfaces;
 
+import spring.demos.car_system.domain.entities.Brand;
+
+import java.time.LocalDateTime;
+
 public interface BrandService {
     //TODO ADD METHODS AND CREATE AN IMPLEMENTATION
+    Brand getByCreationDate(LocalDateTime dateTime);
 }
